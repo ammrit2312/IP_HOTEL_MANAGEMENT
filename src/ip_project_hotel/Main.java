ec094a616c2b0fd28fca50babcabf45d71debe5e
Amritesh <54923120+ammrit2312@users.noreply.github.com>
2020-03-26 01:48:04 +0530

    Add files via upload

---
+++ src/ip_project_hotel/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ip_project_hotel;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author HP
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int day,month,year;
+
+      
+    }
+
+}
